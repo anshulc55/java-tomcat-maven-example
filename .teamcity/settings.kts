306cbb682d43fec7aa71546c3dd39ac4e1d975c5
soumya8108 <32201332+soumya8108@users.noreply.github.com>
2020-06-16 20:13:48 +0530

    test

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -3,15 +3,6 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
-public final class BuildSteps {
-
-fun BuildSteps.buildstep1(name: String) {
-    step {
-        param("name", name) // your parameter here
-        println(name)
-    }
-}
-}
 
 version = "2019.2"
 
@@ -28,14 +19,15 @@ object Build : BuildType({
     }
 
     steps {
-        maven {
+        buildMaven("clean test")
+  /*      maven {
             goals = "clean test"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
-        }
+        }*/
         script {
             scriptContent = "echo Build Successful from soumya"
         }
-        buildstep1("Soumya Prakash Barik")
+        //buildstep1("Soumya Prakash Barik")
     }
 
     triggers {
@@ -43,3 +35,12 @@ object Build : BuildType({
         }
     }
 })
+
+//val soumya = buildMaven("clean test")
+
+fun buildMaven(goal: String) {
+        return maven {
+            goals = goal
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+        }
+}
