ec88bd2458b3c0ad861dc106c518b4972df21b95
soumya8108 <32201332+soumya8108@users.noreply.github.com>
2020-06-17 18:35:51 +0530

    Create _Self.kts

---
+++ .teamcity/patches/projects/_Self.kts
@@ -0,0 +1,12 @@
+package patches.projects
+
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+
+fun helloBoss() {
+println("hello boss ")
+}
+
+
