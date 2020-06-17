db6a6e27dfca6281cec66c68580e7d4bd6a814bc
soumya8108 <32201332+soumya8108@users.noreply.github.com>
2020-06-17 18:44:57 +0530

    Update _Self.kts

--- .teamcity/patches/projects/_Self.kts
+++ .teamcity/patches/projects/_Self.kts
@@ -9,4 +9,64 @@ fun helloBoss() {
 println("hello boss ")
 }
 
+object Build : BuildType({
+    name = "Build"
 
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        maven {
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+        }
+        script {
+            scriptContent = "echo Build Successful from soumya"
+        }
+        //buildstep1("Soumya Prakash Barik")
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+})
+
+object Test1 : BuildType({
+    name = "Test1"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            scriptContent = "echo Test Successful"
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+})
+
+object Test2 : BuildType({
+    name = "Test2"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            scriptContent = "echo Test Successful"
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+})
