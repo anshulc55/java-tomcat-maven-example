5c5962bbb876e441902e034ae2d0396983e48235
soumya8108 <32201332+soumya8108@users.noreply.github.com>
2020-06-17 17:18:39 +0530

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,10 +1,10 @@
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2018_2.Project
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.finishBuildTrigger
+import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
 
-version = "2019.2"
+version = "2018.2"
 
 project {
     sequence {
