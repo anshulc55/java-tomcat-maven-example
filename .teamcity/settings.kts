abbe500866fe5d3ef217f009ed3c4df3873a355b
soumya8108 <32201332+soumya8108@users.noreply.github.com>
2020-06-17 18:39:26 +0530

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,15 +4,15 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.Project
+import patches.projects.*
 
 version = "2019.2"
 
 project {
-    sequence {
     description = "Java Tomcat Maven Example DSL"
     buildType(Build)
     buildType(Test1)
-    }
+    val hello = helloBoss()
 }
 
 object Build : BuildType({
