2309f19d2d114fbf352a905b5ec00593d39ba858
soumya8108 <32201332+soumya8108@users.noreply.github.com>
2020-06-17 18:04:01 +0530

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,40 +1,78 @@
-//settings.kts
+package _Self.vcsRoots
 
-import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.v2018_2.project
-import jetbrains.buildServer.configs.kotlin.v2018_2.version
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
-version = "2018.2"
-
+version = "2019.2"
 
 project {
-    sequence {
-        build(Compile) {
-            produces("application.jar")
-        }
-        build(Test) {
-            requires(Compile, "application.jar")
-            produces("test.reports.zip")
-        }
-        build(Package) {
-            requires(Compile, "application.jar")
-            produces("application.zip")
-        }
-    }
+    description = "Java Tomcat Maven Example DSL"
+    buildType(Build)
+    buildType(Test1)
+    buildType(Test2)
 }
 
-object Compile : BuildType({
-    name = "Compile"
-    //...
+object Build : BuildType({
+    name = "Build"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        maven {
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+        }
+        script {
+            scriptContent = "echo Build Successful from soumya"
+        }
+        //buildstep1("Soumya Prakash Barik")
+    }
+
+    triggers {
+        vcs {
+        }
+    }
 })
 
-object Test : BuildType({
-    name = "Test"
-    //...
+object Test1 : BuildType({
+    name = "Test1"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            scriptContent = "echo Test Successful"
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
 })
 
-object Package : BuildType({
-    name = "Package"
-    //...
+object Test2 : BuildType({
+    name = "Test2"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            scriptContent = "echo Test Successful"
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
 })
