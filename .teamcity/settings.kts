eef3586f4d9316a69780a8ffc9a6927ca8b8110e
soumya8108 <32201332+soumya8108@users.noreply.github.com>
2020-06-17 16:02:38 +0530

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,23 +2,16 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
-/*
-data class buildMaven(val name: String) {
-    fun makeBed() {
-        println("I will make your bed.")
-        maven {
-            goals = "clean test"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
-        }
-    }
-}*/
-val goal: String = "clean test"
+
 
 version = "2019.2"
 
 project {
+    sequence {
     description = "Java Tomcat Maven Example DSL"
     buildType(Build)
+    buildType(Test)
+    }
 }
 
 object Build : BuildType({
@@ -29,12 +22,8 @@ object Build : BuildType({
     }
 
     steps {
-        //val robot1=buildMaven("Soumya prakash barik")
-        //println(robot1.name)
-       // robot1.makeBed()
-    //buildMaven("clean test")
         maven {
-            goals = goal
+            goals = "clean"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
         }
         script {
@@ -49,5 +38,26 @@ object Build : BuildType({
     }
 })
 
-//val soumya = buildMaven("clean test")
+object Build : BuildType({
+    name = "Test"
 
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        maven {
+            goals = "Test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+        }
+        script {
+            scriptContent = "echo Test Successful"
+        }
+        //buildstep1("Soumya Prakash Barik")
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+})
