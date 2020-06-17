8c2a4ae383e4c4bf22603ea8cfa24f1348ff77b7
soumya8108 <32201332+soumya8108@users.noreply.github.com>
2020-06-17 18:51:56 +0530

    Update settings.kts
    variable added

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -3,16 +3,75 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.v2019_2.Project
-import patches.projects.*
 
 version = "2019.2"
+
+mvncommand = "clean test"
 
 project {
     description = "Java Tomcat Maven Example DSL"
     buildType(Build)
     buildType(Test1)
-    //val hello = helloBoss()
 }
 
+object Build : BuildType({
+    name = "Build"
 
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        maven {
+            goals = mvncommand
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+        }
+        script {
+            scriptContent = "echo Build Successful from soumya"
+        }
+        //buildstep1("Soumya Prakash Barik")
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+})
+
+object Test1 : BuildType({
+    name = "Test1"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            scriptContent = "echo Test Successful"
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+})
+
+object Test2 : BuildType({
+    name = "Test2"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            scriptContent = "echo Test Successful"
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+})
