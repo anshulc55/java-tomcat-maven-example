9b2d314614756fa1e3fe95fc619823c8dee0479f
soumya8108 <32201332+soumya8108@users.noreply.github.com>
2020-06-17 18:44:17 +0530

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -12,67 +12,7 @@ project {
     description = "Java Tomcat Maven Example DSL"
     buildType(Build)
     buildType(Test1)
-    val hello = helloBoss()
+    //val hello = helloBoss()
 }
 
-object Build : BuildType({
-    name = "Build"
 
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        maven {
-            goals = "clean test"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
-        }
-        script {
-            scriptContent = "echo Build Successful from soumya"
-        }
-        //buildstep1("Soumya Prakash Barik")
-    }
-
-    triggers {
-        vcs {
-        }
-    }
-})
-
-object Test1 : BuildType({
-    name = "Test1"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        script {
-            scriptContent = "echo Test Successful"
-        }
-    }
-
-    triggers {
-        vcs {
-        }
-    }
-})
-
-object Test2 : BuildType({
-    name = "Test2"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        script {
-            scriptContent = "echo Test Successful"
-        }
-    }
-
-    triggers {
-        vcs {
-        }
-    }
-})
